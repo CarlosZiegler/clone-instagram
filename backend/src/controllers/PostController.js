09d864bcb717e1780174207edee08d79c1838bf1
CarlosZiegler <carlos.ziegler@gmail.com>
2019-06-12 13:13:38 +0200

    methode Store in route /posts

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -9,8 +9,9 @@ module.exports = {
     },
 
     async store (req, res ) {
-
-        
+        console.log(req.body);
+        console.log(req.file);
+        return res.json({OK : true});
 
     }
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,11 @@
 const express =  require('express');
+const PostController = require('./controllers/PostController');
+const multer = require('multer');
+
 const routes = new express.Router();
+const upload = multer();
 
 //cria rota raiz do servidor, tendo uma funcao com parametros req e res: uma requisicao e um resposta
-routes.get('/',(req,res)=> {
-    // enviadno uma response pegando o name como parametro, passado por localhost:3333?name=Carlos
-    return res.send(`Hello ${req.query.name}`);
-
-});
+routes.post('/posts', upload.single('image'), PostController.store);
 
 module.exports = routes;
