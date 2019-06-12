09eb9c1e334e34b0c4978fdad199c4318b237fec
CarlosZiegler <carlos.ziegler@gmail.com>
2019-06-12 09:37:35 +0200

    backend connecte MongoDB

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 //cria um servidos express
 const app = express();
+
+mongoose.connect('mongodb+srv://admin:admin@cluster0-rzeil.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser:true,
+});
 
 //cria rota raiz do servidor, tendo uma funcao com parametros req e res: uma requisicao e um resposta
 app.get('/',(req,res)=> {
