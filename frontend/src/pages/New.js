17f9b021576d2931ad2a071780aca747dd7dea43
CarlosZiegler <carlos.ziegler@gmail.com>
2019-06-14 13:53:59 +0200

    Page New beginnen

--- frontend/src/pages/New.js
+++ frontend/src/pages/New.js
@@ -4,7 +4,36 @@ class New extends Component {
     //metodo render do React
     render(){
         return(
-            <h1>New</h1>
+            <form id="new-post">
+                <input type="file"/>
+
+                <input
+                    type="text"
+                    name="author"
+                    placeholder="Author Post"
+                />
+
+                <input
+                    type="text"
+                    name="place"
+                    placeholder="Place Post"
+                />
+
+                <input
+                    type="text"
+                    name="description"
+                    placeholder="Description Post"
+                />
+
+                <input
+                    type="text"
+                    name="hashtags"
+                    placeholder="Hashtags Post"
+                />
+
+                <button></button>
+
+            </form>
         )
     }
 }
